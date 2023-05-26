08cc6de5c94e96a0f1a742aa134173235944b35c
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-05-27 01:40:00 +0200

    Add files via upload

---
+++ AutoFFEndRound_CursedMod/AutoFfEndRound.cs
@@ -0,0 +1,38 @@
+﻿using CursedMod.Events.Handlers;
+using CursedMod.Features.Wrappers.Server;
+using CursedMod.Loader;
+using CursedMod.Loader.Modules;
+
+namespace AutoFFEndRound_CursedMod;
+
+public class AutoFfEndRound : CursedModule
+{
+    public override string ModuleAuthor => "xNexusACS";
+    public override string ModuleName => "AutoFFEndRound";
+    public override string ModuleVersion => "1.0.0";
+    public override string CursedModVersion => CursedModInformation.Version;
+
+    public override void OnLoaded()
+    {
+        CursedRoundEventsHandler.RoundEnded += OnRoundEnded;
+        CursedRoundEventsHandler.RestartingRound += OnRestartingRound;
+        base.OnLoaded();
+    }
+
+    public override void OnUnloaded()
+    {
+        CursedRoundEventsHandler.RoundEnded -= OnRoundEnded;
+        CursedRoundEventsHandler.RestartingRound -= OnRestartingRound;
+        base.OnUnloaded();
+    }
+    
+    private void OnRoundEnded()
+    {
+        CursedServer.IsFriendlyFireEnabled = true;
+    }
+    
+    private void OnRestartingRound()
+    {
+        CursedServer.IsFriendlyFireEnabled = false;
+    }
+}
